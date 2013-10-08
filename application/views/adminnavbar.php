6fb0bae1892db26d467e6ab52c01bef2e87903e2
Joe Angel <joe10330@gmail.com>
2013-10-08 17:44:44 +0800

    Create adminnavbar.php

---
+++ application/views/adminnavbar.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?=$title; ?></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="Bootstrap">
+	<meta name="description" content="Bootstrap">
+	<meta name=”robots” content=”noodp,noydir”>
+	<link rel="shortcut icon" href="<?=base_url(); ?>favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/css/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/css/aexree.css" media="screen">
+	<style type="text/css">
+	header{
+	height:100px;
+	}
+#nav.affix {
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+#nav {
+    position:relative;
+}
+
+	</style>
+</head>
+<body data-spy="scroll" data-target=".navbar">
+
+<header> 
+<div class="well"> 
+	<h2>Header</h2>
+</div>
+</header>
+
+
+
+
+<div class="container">
+<div class="row">
+
+<div id="nav-wrapper">
+<div id="nav" class="navbar affix-top" data-spy="affix">
+  <div class="navbar-inner" data-spy="affix-top">
+    <div class="container">
+
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <!-- Everything you want hidden at 940px or less, place within here -->
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#service-top">Services</a></li>
+            <li><a href="#contact-arrow">Contact</a></li>
+        </ul><!--/.nav-->
+      </div><!--/.nav-collapse collapse pull-right-->
+    </div><!--/.container-->
+  </div><!--/.navbar-inner-->
+</div>
+</div><!--/#nav /.navbar navbar-inverse-->
+
+
+
+
+	<div class="span3">
+<div class="nav">
+<ul>
+<li class="nav-header">
+  <a href="#" data-toggle="collapse" data-target="#userMenu">
+    User <i class="icon-angle-down"></i>
+  </a>
+  <ul style="list-style: none;" class="collapse in" id="userMenu">
+    <li class="active"><a href="#"><i class="icon-home"></i>Home</a></li>
+    <li><a href="#"><i class="icon-envelope-alt"></i>Messages <span class="badge badge-info">4</span></a></li>
+    <li><a href="#"><i class="icon-cogs"></i>Settings</a></li>
+    <li><a href="#"><i class="icon-comment"></i>Shoutbox</a></li>
+    <li><a href="#"><i class="icon-user"></i>Staff List</a></li>
+    <li><a href="#"><i class="icon-flag"></i>My Infractions</a></li>
+    <li><a href="#"><i class="icon-exclamation-sign"></i>Rules &amp; Regulations</a></li>
+    <li><a href="#"><i class="icon-off"></i>Logout</a></li>
+  </ul>
+</li>
+<li class="nav-header">
+  <a href="#" data-toggle="collapse" data-target="#radioDJMenu">
+    Radio DJ <i class="icon-angle-right"></i>
+  </a>
+  <ul style="list-style: none;" class="collapse" id="radioDJMenu">
+    <li><a href="#">Information &amp; Stats</a></li>
+    <li><a href="#">DJ Says</a></li>
+    <li><a href="#">Request Line</a></li>
+    <li><a href="#">Timetable</a></li>
+    <li><a href="#">Banned Songs</a></li>
+  </ul>
+</li>
+<li class="nav-header">
+<a href="#" data-toggle="collapse" data-target="#headDJMenu">
+    Head DJ <i class="icon-angle-right"></i>
+  </a>
+  <ul style="list-style: none;" class="collapse" id="headDJMenu">
+    <li><a href="#"><i class="icon-"></i>All Banned Songs</a></li>
+  </ul>
+</li>
+</ul>
+</div>
+	</div>
+	<div class="span9">
+		<h1>Main</h1>
+		Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+
+		<br>
+
+		Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+
+		<br>
+
+		Qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+
+		<br>
+		    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+    
+		<br>
+		    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+    
+		<br>
+		    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+    
+		<br>
+		    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+    
+		<br>
+		    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+    
+		<br>
+		    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+    
+		<br>
+		    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+		totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
+		dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
+		eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+		sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+    
+	</div>
+</div>
+</div>
+
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="<?=base_url(); ?>assets/js/bootstrap.min.js"></script>
+<script>
+$('[data-toggle=collapse]').click(function(){
+	
+  	// toggle icon
+  	$(this).find("i").toggleClass("icon-angle-right icon-angle-down");
+  
+});
+
+$('.collapse').on('show', function (e) {
+  
+  	// hide open menus
+  	$('.collapse').each(function(){
+      if ($(this).hasClass('in')) {
+          $(this).collapse('toggle');
+      }
+    });
+  
+})
+    $(document).ready(function() {
+        $('#nav-wrapper').height($("#nav").height());
+        $('#nav').affix({
+            offset: $('#nav').position()
+        });
+    });
+</script>
+</body>
+</html>
